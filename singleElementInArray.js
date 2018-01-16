bfd1298b2130134624952807fbab2c81ffddd4e6
Carter Chavez <Carter.chavez17@gmail.com>
2018-01-16 15:10:34 -0800

    singleElementInArray

---
+++ singleElementInArray.js
@@ -0,0 +1,22 @@
+/*
+Given a sorted array consisting of only integers where every element appears twice except for 
+one element which appears once. Find the single element that appears once.
+
+Constraints: 
+	Time: O(log n)
+	Space: O(1)
+
+Example: 
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+*/
+
+
+const singleNonDuplicate = (nums) => {
+	for ( let i=0; i<nums.length; i+=2 ) {
+		if ( nums[i] !== nums[i+1] ) {
+			return nums[i]
+		}
+	}
+};
+
