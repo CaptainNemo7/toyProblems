77c5744dba293efc3eeda655f9e75ec4d723e6d8
Carter Chavez <Carter.chavez17@gmail.com>
2018-01-28 14:51:22 -0800

    implement a queue

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -22,3 +22,32 @@ Stack.prototype.pop = function() {
 Stack.prototype.minNumber = function() {
 	return this.min;
 }
+
+// implement a Queue using two stacks;
+
+function Queue() {
+	this.stack1 = new Stack();
+	this.stack2 = new Stack();
+}
+
+Queue.prototype.unshift = function(val) {
+	this.stack1.push(val);
+}
+
+Queue.prototype.shift = function() {
+	if ( this.stack2.count === 0 ) {
+		while ( this.stack1.count > 0 ) {
+			this.stack2.push(this.stack1.pop())
+		}
+		return this.stack2.pop();
+	} else {
+		return this.stack2.pop()
+	}
+}
+
+
+
+
+
+
+
