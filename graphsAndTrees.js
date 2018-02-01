99133520bb3e2dba9668a98b2caa444d804f6692
Carter Chavez <Carter.chavez17@gmail.com>
2018-01-31 21:51:21 -0800

    create bst

--- graphsAndTrees.js
+++ graphsAndTrees.js
@@ -19,3 +19,30 @@ const isEdgeOf = (node, target) => {
   }
   return false;
 };
+
+// 4.2 given an array of increasing integers, create a BST with minimal height;
+// function Tree(val) {
+//   this.val = val;
+//   this.left = this.right = null;
+// }
+
+const makeTree = arr => {
+  
+  const nextLevel = (start, end) => {
+    if ( end < start ) {
+      return null;
+    }
+    let mid = Math.floor((start + end)/2)
+    let n = new Tree(arr[mid]);
+    n.left = nextLevel(start, mid-1);
+    n.right = nextLevel(mid+1, end);
+    return n;
+  }
+  
+  return nextLevel(0, arr.length-1);
+ 
+}
+
+
+
+
