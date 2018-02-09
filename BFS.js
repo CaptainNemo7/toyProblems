692ef58c9da5f19ffd9563836e6483fb71e8c517
Carter Chavez <Carter.chavez17@gmail.com>
2018-02-08 20:32:15 -0800

    upgrade

--- BFS.js
+++ BFS.js
@@ -1,3 +1,31 @@
+//BFS for target
+
+const findTargetBF = (root, target) => {
+  let queue = [root];
+
+  while ( queue.length > 0 ) {
+    let node = queue.shift();
+
+    if ( node.value === target ) {
+      return node;
+    }
+
+    if ( node.left ) {
+      queue.push(node.left);
+    }
+
+    if ( node.right ) {
+      queue.push(node.right);
+    }
+
+  }
+
+  return false;
+}
+
+
+
+
 const traverseBreadthFirst = (root, cb) => {
   let queue = [root];
 
--- DFS.js
+++ DFS.js
@@ -1,24 +1,101 @@
-const traverseDepthSearch = (root, cb) => {
-	let stack = [root];
-	while ( stack.length > 0 ) {
-		let node = stack.pop();
-		cb(node.value);
-		if ( node.right ) {
-			stack.push(node.right);
-		} 
-		if ( node.left ) {
-			stack.push(node.left);
-		}
-	}
-}
+// const Node = function(value) {
+//   this.value = value;
+//   this.left = null;
+//   this.right = null;
+// }
+
+// let nodeA = new Node('1');
+// let nodeB = new Node('2');
+// let nodeC = new Node('3');
+// let nodeD = new Node('4');
+// let nodeE = new Node('5');
+// let nodeF = new Node('6');
+// let nodeG = new Node('7');
+// let nodeH = new Node('8');
+
+// nodeA.left = nodeB;
+// nodeA.right = nodeC;
+// nodeC.left = nodeD;
+// nodeC.right = nodeF;
+// nodeD.right = nodeE;
+// nodeF.left = nodeG;
+// nodeF.right = nodeH;
+
+// const printNode = function(node) {
+//   console.log(node.value * 2);
+// }
+// traverseDepthFirst(nodeA, printNode);
+
+const traverseDepthFirst = function(root, callback) {
+  let stack = [root];
+  
+  while ( stack.length > 0 ) {
+    let node = stack.pop();
+    callback(node);
+    
+    if ( node.right ) {
+      stack.push(node.right);
+    }
+    
+    if ( node.left ) {
+      stack.push(node.left);
+    }
+  }
+};
+
 
 // W/ recursion
-const traverseDepthSearch = (root, cb) => {
-  cb(root.value);
-  if ( root.left ) {
-    traverseDepthSearch(root.left, cb);
+const traverseDepthFirst = function(node, callback) {
+  callback(node);
+  
+  if ( node.left ) {
+    traverseDepthFirst(node.left, callback);
   }
-  if ( root.right ) {
-    traverseDepthSearch(root.right, cb);
+  
+  if ( node.right ) {
+    traverseDepthFirst(node.right, callback)
+  }
+};
+
+
+
+const searchDepthFirst = function(root, targetValue) {
+  let stack = [root];
+  
+  while ( stack.length > 0 ) {
+    let node = stack.pop();
+    
+    if ( node.value === targetValue ) {
+      return node;
+    }
+    
+    if ( node.right ) {
+      stack.push(node.right);
+    }
+    
+    if ( node.left ) {
+      stack.push(node.left);
+    }
   }
 }
+
+// w/ recursion
+const searchDepthFirst = function(node, targetValue) {
+  let left = null;
+  let right = null;
+  
+  if ( node.value === targetValue ) {
+    return node;
+  }
+  
+  if ( node.left ) {
+    left = searchDepthFirst(node.left, targetValue);
+  }
+  
+  if ( node.right ) {
+    right = searchDepthFirst(node.right, targetValue);
+  }
+  
+  return left || right;
+};
+
